54d14de8ac0c1ee7e6bb8404400c6d5a6bbabf15
Abylay Ospan <aospan@netup.ru>
2016-09-07 12:34:47 -0400

    vaapidecodebin instead vaapidecode ffmpeg added
    Signed-off-by: Abylay Ospan <aospan@netup.ru>

--- opt/transode-file.sh
+++ opt/transode-file.sh
@@ -5,7 +5,7 @@ time /opt/gstreamer-dist-master/bin/gst-launch-1.0 \
     -v --gst-debug-level=0 --gst-debug-no-color \
     filesrc location=/opt/moscow24.ts ! queue ! tsdemux name=tdemux \
     tdemux. ! queue ! audio/mpeg ! mpegaudioparse ! avdec_mp3 ! audioresample ! audioconvert ! faac bitrate=64000 ! queue ! tmux. \
-    tdemux. ! queue ! mpegvideoparse ! vaapidecode ! \
+    tdemux. ! queue ! mpegvideoparse ! vaapidecodebin ! \
     vaapipostproc force-aspect-ratio=false scale-method=2 width=320 height=240 deinterlace-method=2 deinterlace-mode=1 ! \
     video/x-raw,interlace-mode=progressive ! \
     vaapih264enc bitrate=256 tune=1 rate-control=2 ! \
